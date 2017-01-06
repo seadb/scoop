de5c2e0a26b93a33f1ecd721229e63cf8bd9f4f4
Chelsea Bridson <bridsonc@msu.edu>
2017-01-06 15:59:56 -0500

    update config

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,42 +1,40 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-const users = require('../user/user-repository')
+const userModel = require('../user/user-model')
+const Users = new userModel();
 
-const comparePassword = (userPassword, databasePassword) => {
-  return bcrypt.compareSync(userPassword, databasePassword);
-}
-const configPassport = function (passport) {
+const configPassport = function () {
 
   passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
 
   passport.deserializeUser(function (id, done) {
-    users.getUserById(id)
+    Users.getUserById(id)
     .then((user) => { done(null, user); })
     .catch((err) => { done(err,null); });
   });
-  
-  passport.use('local-login', new LocalStrategy({
+
+  // 'local' login
+  passport.use(new LocalStrategy({
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function (email, password, done) {
       // check to see if the username exists
-      users.getUserByEmail(email)
-      .then(function (user) {
-        if (!user) return done(null, false);
-        if (!comparePassword(password, user.password)) {
-          return done(null, false);
-        }
-        else {
-          return done(null, user);
-        }
-    })
-    .catch(function (err) { return done(err); });
-  }));
+      Users.getUserByEmail(email)
+        .then(function (user) {
+          if (!user) return done(null, false);
+          if (!comparePassword(password, user.password)) {
+            return done(null, false);
+          }
+          else {
+            return done(null, user);
+          }
+      })
+      .catch(function (err) { return done(err); });
+    }
+  ));
 
 }
 
