73842ea8b8dba31592f618e435894587ff5015b2
Chelsea Bridson <bridsonc@msu.edu>
2017-01-06 22:07:42 -0500

    added authentication module (login/register) !TODO: logout, isAuthenticated middleware - auth/auth-controller.js   implemented login and register methods - auth/auth-routes.js    /login /logout and /register - package.json   added jsonwebtoken - routes.js   use auth routes - server.js   clean up a couple lines

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,13 @@
 const routes = require('express').Router();
-const users = require('./user/user-routes');
-const user = require('./user/user-model');
+const users  = require('./user/user-routes');
+const auth   = require('./auth/auth-routes');
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' });
 });
 
 routes.use('/api/users', users);
+routes.use('/api/auth', auth);
 
 module.exports = routes;
 
