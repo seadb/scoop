dfd4aeaa5b729bd4b421cb566f20f8a6b953dde1
Chelsea Bridson <bridsonc@msu.edu>
2017-01-06 22:52:40 -0500

    remove unused code

--- server/auth/auth-routes.js
+++ server/auth/auth-routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var routes = express.Router();
 var controller = require('./auth-controller.js');
-var bodyParser = require('body-parser');
 
 routes.post('/login', controller.login);
 routes.post('/logout', controller.logout);
--- server/user/user.sql
+++ server/user/user.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY,
-  "first_name" VARCHAR(255),
-  "last_name" VARCHAR(255),
-  "email" VARCHAR(255),
-  "password" VARCHAR(2048),
-  "bio" VARCHAR(1024),
-  "age" INTEGER,
-  "sex" VARCHAR,
-  "created" timestamp default current_timestamp
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(2048),
+  bio VARCHAR(1024),
+  age INTEGER,
+  sex VARCHAR,
+  created timestamp default current_timestamp
 );
